ea52fdeeb9da9ff11c3a9bc6fa0eaa834e04b8a8
Vivek Patil <vivekrp4503@gmail.com>
2024-07-23 00:25:10 +0530

    Create URL Dialog

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,6 +17,7 @@ import { getUrls } from '@/db/apiUrls'
 import { UrlState } from '@/context'
 import { getClicksForUrls } from '@/db/apiCliks'
 import LinkCard from '@/components/LinkCard'
+import CreateLink from '@/components/CreateLink'
   
 const Dashboard = () => {
 
@@ -72,7 +73,7 @@ const Dashboard = () => {
             </div>
             <div className='flex justify-between'>
                 <h1 className=' text-4xl font-extrabold'>My Links</h1>
-                <Button>Create Link</Button>
+                <CreateLink/>
             </div>
 
             <div className=' relative'>
