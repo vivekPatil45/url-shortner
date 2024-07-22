fffd991c0354956adf6a070eecc5ec395916aa61
Vivek Patil <vivekrp4503@gmail.com>
2024-07-23 00:48:42 +0530

    Link Redirection Page, Statistics API,Redirection Logic

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -17,7 +17,20 @@ export async function getUrls(user_id){
 
 }
 
-
+export async function getLongUrl(id) {
+    let {data: shortLinkData, error: shortLinkError} = await supabase
+      .from("urls")
+      .select("id, original_url")
+      .or(`short_url.eq.${id},custom_url.eq.${id}`)
+      .single();
+  
+    if (shortLinkError && shortLinkError.code !== "PGRST116") {
+      console.error("Error fetching short link:", shortLinkError);
+      return;
+    }
+  
+    return shortLinkData;
+  }
 
 export async function createUrl({title, longUrl, customUrl, user_id}, qrcode) {
     const short_url = Math.random().toString(36).substr(2, 6);
--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { storeClicks } from '@/db/apiCliks';
+import { getLongUrl } from '@/db/apiUrls';
+import useFetch from '@/hooks/useFetch';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { BarLoader } from 'react-spinners';
 
 const RedirectLink = () => {
-    return (
-        <div>
-            RedirectLink
-        </div>
-    )
+    const{id} = useParams();
+    const {loading, data, fn} = useFetch(getLongUrl, id);
+
+    const {loading: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+        id: data?.id,
+        originalUrl: data?.original_url,
+    });
+    
+    useEffect(() => {
+        fn();
+      }, []);
+    
+      useEffect(() => {
+        if (!loading && data) {
+          fnStats();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [loading]);
+    
+      if (loading || loadingStats) {
+        return (
+          <>
+            <BarLoader width={"100%"} color="#36d7b7" />
+            <br />
+            Redirecting...
+          </>
+        );
+      }
+    
+      return null;
 }
 
 export default RedirectLink
