98845df9db80e39de52019fb9b76afe767f1fbe0
Vivek Patil <vivekrp4503@gmail.com>
2024-07-22 23:33:19 +0530

    Create URL API

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 
 export async function getUrls(user_id){
@@ -15,6 +15,42 @@ export async function getUrls(user_id){
 
     return data;
 
+}
+
+
+
+export async function createUrl({title, longUrl, customUrl, user_id}, qrcode) {
+    const short_url = Math.random().toString(36).substr(2, 6);
+    const fileName = `qr-${short_url}`;
+
+    const {error: storageError} = await supabase.storage
+        .from("qrs")
+        .upload(fileName, qrcode);
+
+    if (storageError) throw new Error(storageError.message);
+
+    const qr = `${supabaseUrl}/storage/v1/object/public/qrs/${fileName}`;
+  
+    const {data, error} = await supabase
+        .from("urls")
+        .insert([
+            {
+            title,
+            user_id,
+            original_url: longUrl,
+            custom_url: customUrl || null,
+            short_url,
+            qr,
+            },
+        ])
+        .select();
+  
+    if (error) {
+        console.error(error);
+        throw new Error("Error creating short URL");
+    }
+  
+    return data;
 }
 
 export async function deleteUrl(id) {
