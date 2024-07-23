7fba713647b2e363b81f318a9ca00638c4e6f2f4
Vivek Patil <vivekrp4503@gmail.com>
2024-07-23 15:31:03 +0530

    complete

--- src/pages/LinkPage.jsx
+++ src/pages/LinkPage.jsx
@@ -57,7 +57,7 @@ const LinkPage = () => {
 
     let link ="";
     if(url){
-        link = url?.custom_url ? url?.custom_ur : url.short_url;
+        link = url?.custom_url ? url?.custom_url : url.short_url;
     }
 
     return (
